1976f97929b679bdf23bb9df622809f0a24dfbf4
Hedius <git@hedius.eu>
2024-01-01 00:35:26 +0100

    add debian build support

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name='clickhouse_backup',
@@ -6,13 +12,16 @@ setup(
     url='https://github.com/Hedius/clickhouse-backup',
     license='GPLv3',
     author='Hedius',
-    author_email='git@hedius.eu',
+    author_email='clickhouse-backup@hedius.eu',
     description='A python wrapper around ClickHouse to use the BACKUP '
                 'command for creating backups of your database.',
-    packages=['clickhouse_backup'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    packages=find_namespace_packages(include=['clickhouse_backup.*']),
     entry_points={
         'console_scripts': ['clickhouse-backup=clickhouse_backup.clickhouse_backup:main'],
     },
+    install_requires=requirements,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: System Administrators',
