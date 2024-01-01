dfba0e354b02cd7b347a947e9fb65c9766420dd2
Hedius <git@hedius.eu>
2024-01-01 01:52:03 +0100

    fix deb command wrapper

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_namespace_packages
 
 from pathlib import Path
-root = Path(__file__).parent
-with open(root / 'requirements.txt') as f:
-    requirements = f.read().splitlines()
 
-with open(root / 'README.md') as f:
+with open(Path(__file__).parent / 'README.md') as f:
     readme = f.read()
 
 setup(
@@ -15,15 +12,21 @@ setup(
     license='GPLv3',
     author='Hedius',
     author_email='clickhouse-backup@hedius.eu',
+    maintainer='Hedius',
+    maintainer_email='clickhouse-backup@hedius.eu',
     description='A python wrapper around ClickHouse to use the BACKUP '
                 'command for creating backups of your database.',
     long_description=readme,
     long_description_content_type='text/markdown',
-    packages=find_namespace_packages(include=['clickhouse_backup.*']),
+    packages=find_namespace_packages(include=['clickhouse_backup', 'clickhouse_backup.*']),
     entry_points={
-        'console_scripts': ['clickhouse-backup=clickhouse_backup.run:main'],
+        'console_scripts': ['clickhouse-backup=clickhouse_backup.backup:main'],
     },
-    install_requires=requirements,
+    install_requires=[
+        'clickhouse-driver>=0.2.6',
+        'dynaconf>=3.2.4',
+        'loguru>=0.7.2'
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: System Administrators',
